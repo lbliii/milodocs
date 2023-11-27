578611d45c6c6eb763e92d748b0db96a752b166b
L.B <lawrence.lane@hpe.com>
2023-11-27 12:12:55 -0500

    img widths

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -28,8 +28,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
     const isDarkMode = document.documentElement.classList.contains("dark");
 
     darkModeToggle.innerHTML = isDarkMode
-      ? '<img src="/icons/dark.svg" aria-lable="activate lightmode" alt="moon">'
-      : '<img src="/icons/light.svg" aria-lable="activate darkmode" alt="sun">';
+      ? '<img src="/icons/dark.svg" aria-lable="activate lightmode" class="w-5" alt="moon">'
+      : '<img src="/icons/light.svg" aria-lable="activate darkmode" class="w-5" alt="sun">';
   }
 
   function updateSectionIcons() {
