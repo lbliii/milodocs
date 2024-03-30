480dd8925957c46644e606a49940c79d58715f2d
L.B <lawrence.lane@hpe.com>
2024-03-30 17:59:51 -0400

    opensource color variant options

--- tools/env.sh
+++ tools/env.sh
@@ -6,9 +6,6 @@ ENVIRONMENTS="production, development, offline, enterprise, opensource" # Add or
 # Usage message
 USAGE="Usage: sh environment.sh [${ENVIRONMENTS}]"
 
-## Pass in one of the specified product versions to this script to start the Hugo server with that version.
-## e.g., sh product.sh prod1
-
 # Check if an argument is provided
 if [ $# -eq 0 ]; then
     echo "No environment specified. Only use this command to start the hugo server with a specific config/{environment}.yaml file."
@@ -17,46 +14,40 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
-# Check if the provided argument is a valid product version
-if ! [[ "${PRODUCT_VERSIONS}" =~ (^|[[:space:]])"$1"($|[[:space:]]) ]]; then
-    echo "Invalid product version specified."
+# Check if the provided argument is a valid environment
+if ! [[ "${ENVIRONMENTS}" =~ (^|[[:space:],])"$1"($|[[:space:],]) ]]; then
+    echo "Invalid environment specified."
     echo "-- ${USAGE}"
     exit 1
 fi
 
 # If $1 is all or ALL, do not set the HUGO_ORG environment variable
-if [ $1 = "all" ] || [ $1 = "ALL" ]; then
+if [[ $1 == "all" ]] || [[ $1 == "ALL" ]]; then
     echo "Starting Hugo server for all Docs"
     hugo server
     exit 0
-else if [ $1 ="offline"]; then
+elif [[ $1 == "offline" ]]; then
     echo "Starting Hugo server for offline Docs"
     hugo server --config config/offline.yaml --environment offline
     exit 0
-
-else if [ $1 ="enterprise"]; then
+elif [[ $1 == "enterprise" ]]; then
     echo "Starting Hugo server for enterprise Docs"
     hugo server --config config/enterprise.yaml --environment enterprise
     exit 0
-
-else if [ $1 ="opensource"]; then
+elif [[ $1 == "opensource" ]]; then
     echo "Starting Hugo server for opensource Docs"
-    hugo server --config config/opensource.yaml --environment opensource
+    hugo server --config config/open-source.yaml --environment opensource
     exit 0
-
-else if [ $1 ="production"]; then
+elif [[ $1 == "production" ]]; then
     echo "Starting Hugo server for production Docs"
-    hugo server  --environment production
+    hugo server --environment production
     exit 0
-
-else if [ $1 ="development"]; then
+elif [[ $1 == "development" ]]; then
     echo "Starting Hugo server for development Docs"
-    hugo server  --environment development
+    hugo server --environment development
     exit 0
-
 else
-
-    # Set the HUGO_ORG environment variable for other valid product versions
+    # Set the HUGO_ORG environment variable for other valid environments
     export HUGO_PRODUCT=$1
     export HUGO_TITLE="$1 Docs"
 
